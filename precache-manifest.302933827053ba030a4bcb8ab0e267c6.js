ad564e4b788649ced3a90ac0e3ba46e839e59662
Amar Rampersaud <arampers94@gmail.com>
2019-08-24 14:37:11 -0400

    Updates

---
+++ precache-manifest.302933827053ba030a4bcb8ab0e267c6.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "45bb435669c7f54c30db44651ad5d9d5",
+    "url": "/Portfolio/index.html"
+  },
+  {
+    "revision": "1612db5ff15872616ffd",
+    "url": "/Portfolio/static/css/2.819eb4d1.chunk.css"
+  },
+  {
+    "revision": "711a08e3cd9c7f39fd64",
+    "url": "/Portfolio/static/css/main.6a96436f.chunk.css"
+  },
+  {
+    "revision": "1612db5ff15872616ffd",
+    "url": "/Portfolio/static/js/2.ee7c8e53.chunk.js"
+  },
+  {
+    "revision": "711a08e3cd9c7f39fd64",
+    "url": "/Portfolio/static/js/main.62e9ba0f.chunk.js"
+  },
+  {
+    "revision": "02bb7fa0c0195f9aa52b",
+    "url": "/Portfolio/static/js/runtime~main.aeacf694.js"
+  },
+  {
+    "revision": "fdc4d16925f48ff53574dabaad8e06f0",
+    "url": "/Portfolio/static/media/capture.fdc4d169.gif"
+  },
+  {
+    "revision": "c21b41e77dc166e56cdd35876cc4a7a2",
+    "url": "/Portfolio/static/media/oculus.c21b41e7.jpg"
+  },
+  {
+    "revision": "73e7cf23a719504454d5dcab5cd86cd1",
+    "url": "/Portfolio/static/media/portfolio-landing-page.73e7cf23.png"
+  },
+  {
+    "revision": "12f3c1b8b6f5347209d7e7decd28de16",
+    "url": "/Portfolio/static/media/project-1.12f3c1b8.png"
+  },
+  {
+    "revision": "2244ba7c239d22af31e61fc005eb2017",
+    "url": "/Portfolio/static/media/robot.2244ba7c.MOV"
+  }
+]);
